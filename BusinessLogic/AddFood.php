f1c4802dd3538e7d9df77867691faa7ba43f57ae
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-05 09:54:19 +0700

    update AddFood.php menambah makanan

--- BusinessLogic/AddFood.php
+++ BusinessLogic/AddFood.php
@@ -2,5 +2,11 @@
 
   function addFood(string $name, int $price): void
   {
-    
+    global $foods;
+
+    $number = is_array($foods) ? sizeof($foods) : 0;
+    $number = $number + 1;
+
+    $foods[$number]["name"] = $name;
+    $foods[$number]["price"] = $price;
   }
