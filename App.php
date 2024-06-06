e5be12369040ff3b971e8e9ed1f5d89545efe215
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-06 10:43:55 +0700

    update App.php require ViewMainMenu.php and call viewMainMenu function

--- App.php
+++ App.php
@@ -7,5 +7,8 @@
   require_once __DIR__ . "/View/ViewShowFood.php";
   require_once __DIR__ . "/View/ViewAddFood.php";
   require_once __DIR__ . "/View/ViewRemoveFood.php";
+  require_once __DIR__ . "/View/ViewMainMenu.php";
 
   echo "Cafetaria App" . PHP_EOL;
+
+  viewMainMenu();
