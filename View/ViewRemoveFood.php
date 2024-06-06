5a7ece94d6668c94af6adc8b3b8c86b7e32f5b88
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-06 09:46:51 +0700

    update ViewRemovFood.php proses menghapus makanan

--- View/ViewRemoveFood.php
+++ View/ViewRemoveFood.php
@@ -1,6 +1,32 @@
 <?php
 
+  require_once __DIR__ . "/../BusinessLogic/RemoveFood.php";
+  require_once __DIR__ . "/../Helper/Input.php";
+
   function viewRemoveFood(): void
   {
-    
+    global $foods;
+
+    echo "MENGHAPUS MAKANAN" . PHP_EOL;
+
+    $number = input("Nomor (x untuk batal)");
+
+    if($number == "x")
+    {
+      "Batal menghapus makanan" . PHP_EOL;
+    }else if(!is_numeric($number))
+    {
+      echo "Gagal menghapus makanan, nomor harus bilangan" . PHP_EOL;
+    }else
+    {
+      $success = removeFood($number);
+
+      if($success)
+      {
+        echo "Sukses menghapus makanan $number" . PHP_EOL;
+      }else
+      {
+        echo "Gagal menghapus makanan $number" . PHP_EOL;
+      }
+    }
   }
