7968c585b7c6cad07ec4ea32064e20e9b1a29328
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-07 10:41:12 +0700

    update AddDrink.php menambah minuman

--- BusinessLogic/AddDrink.php
+++ BusinessLogic/AddDrink.php
@@ -2,5 +2,11 @@
 
   function addDrink(string $name, int $price): void
   {
-    
+    global $drinks;
+
+    $number = is_array($drinks) ? sizeof($drinks) : 0;
+    $number = $number + 1;
+
+    $drinks[$number]["name"] = $name;
+    $drinks[$number]["price"] = $price;
   }
