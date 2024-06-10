0a25ea8ecd6724f42fbaca3316ae7e54a2178610
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-10 18:20:50 +0700

    update AddOrder.php menambah pemesanan

--- BusinessLogic/AddOrder.php
+++ BusinessLogic/AddOrder.php
@@ -2,5 +2,14 @@
 
   function addOrder(int $code, string $name, int $price, int $qty): void
   {
-    
+    global $orders;
+
+    $number = is_array($orders) ? sizeof($orders) : 0;
+    $number = $number + 1;
+
+    $orders[$number]["code"] = $code;
+    $orders[$number]["name"] = $name;
+    $orders[$number]["price"] = $price;
+    $orders[$number]["qty"] = $qty;
+    $orders[$number]["sub_total"] = $orders[$number]["price"]*$orders[$number]["qty"];
   }
