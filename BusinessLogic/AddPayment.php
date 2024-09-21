471adbd619cbc1befda8c2c38a730dab9c262b48
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-21 08:12:48 +0700

    update AddPayment.php menambah pembayaran

--- BusinessLogic/AddPayment.php
+++ BusinessLogic/AddPayment.php
@@ -2,5 +2,13 @@
 
   function addPayment(int $code, int $total, int $pay): void
   {
+    global $payments;
 
+    $number = is_array($payments) ? sizeof($payments) : 0;
+    $number = $number + 1;
+
+    $payments[$number]['code'] = $code;
+    $payments[$number]['total'] = $total;
+    $payments[$number]['pay'] = $pay;
+    $payments[$number]['change'] = $pay-$total;
   }
