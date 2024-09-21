52146cb4aea2ba9b3d1fd191e842fdda3c3ecd0e
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-21 08:08:11 +0700

    update ShowPayment.php menampilkan daftar pembayaran

--- BusinessLogic/ShowPayment.php
+++ BusinessLogic/ShowPayment.php
@@ -2,5 +2,21 @@
 
   function showPayment(): void
   {
-    
+    global $payments;
+
+    echo "DAFTAR PEMBAYARAN" . PHP_EOL;
+
+    if(!empty($payments))
+    {
+      $counter = 0;
+      foreach($payments as $payment)
+      {
+        $counter++;
+        echo "$counter. Kode: " . $payment["code"] . "  Total: " . $payment["total"] .
+        "  Jumlah Bayar: " . $payment["pay"] . "  Kembalian: " . $payment["change"] . PHP_EOL;
+      }
+    }else
+    {
+      echo "Tidak ada daftar pembayaran" . PHP_EOL;
+    }
   }
