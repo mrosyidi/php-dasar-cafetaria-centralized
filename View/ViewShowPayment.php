fa725a0ba7a309684e94307d81df9523c4fb2f42
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-21 14:36:42 +0700

    update ViewShowPayment.php menampilkan daftar pesanan yang akan di proses dan menu

--- View/ViewShowPayment.php
+++ View/ViewShowPayment.php
@@ -1,6 +1,31 @@
 <?php
 
+  require_once __DIR__ . "/../BusinessLogic/ShowOrder.php";
+  require_once __DIR__ . "/../Helper/Input.php";
+  require_once __DIR__ . "/ViewMainMenu.php";
+
   function viewShowPayment(): void
   {
-    
+    showOrder();
+
+    while(true)
+    {
+      echo "Menu Pembayaran" . PHP_EOL;
+      echo "1. Bayar Pesanan" .  PHP_EOL;
+      echo "x. Kembali" . PHP_EOL;
+
+      $pilihan = input("Pilih");
+
+      if($pilihan == "1")
+      {
+
+      }else if($pilihan == "x")
+      {
+        viewMainMenu();
+        exit();
+      }else
+      {
+        echo "Pilihan tidak dimengerti" . PHP_EOL;
+      }
+    }
   }
