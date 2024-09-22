be774d2d948e0fb6a7d9bb81ba51034ccf16de25
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-22 07:38:51 +0700

    update AddDetail.php menambah detail

--- BusinessLogic/AddDetail.php
+++ BusinessLogic/AddDetail.php
@@ -2,5 +2,18 @@
 
   function addDetail(array $elements): void
   {
-    
+    global $details;
+
+    $number = is_array($details) ? sizeof($details) : 0;
+    $number = $number + 1;
+
+    for($index = 0; $index < count($elements); $index++)
+    {
+      $details[$number]['code'] = $elements[$index]['code'];
+      $details[$number]['name'] = $elements[$index]['name'];
+      $details[$number]['price'] = $elements[$index]['price'];
+      $details[$number]['qty'] = $elements[$index]['qty'];
+      $details[$number]['sub_total'] = $elements[$index]['sub_total'];
+      $number++;
+    }
   }
