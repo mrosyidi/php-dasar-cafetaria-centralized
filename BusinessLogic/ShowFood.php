70bcd0eac4bbc528fdbd297941333ad710d61d69
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-11-10 04:07:28 +0700

    update ShowFood.php menampilkan daftar makanan

--- BusinessLogic/ShowFood.php
+++ BusinessLogic/ShowFood.php
@@ -2,5 +2,18 @@
 
     function showFood(): void 
     {
+        global $foods;
 
+        echo "DAFTAR MAKANAN" . PHP_EOL;
+
+        if(empty($foods))
+        {
+            echo "Tidak ada daftar makanan" . PHP_EOL;
+        }else 
+        {
+            foreach($foods as $number => $food)
+            {
+                echo "$number. " . $food['name'] . " Rp." . $food['price'] . PHP_EOL;
+            }
+        }
     }
