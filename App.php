1f8472845ce2bdc933a0be14a22873b0cc653b32
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-11-22 22:57:13 +0700

    update App.php menampilkan menu utama

--- App.php
+++ App.php
@@ -7,5 +7,44 @@
     require_once __DIR__ . "/View/ViewShowFood.php";
     require_once __DIR__ . "/View/ViewAddFood.php";
     require_once __DIR__ . "/View/ViewRemoveFood.php";
+    require_once __DIR__ . "/Helper/Input.php";
 
     echo "Cafetaria App" . PHP_EOL;
+
+    while(true)
+    {
+        echo "MENU UTAMA" . PHP_EOL;
+        echo "1. Daftar Makanan" . PHP_EOL;
+        echo "2. Daftar Minuman" . PHP_EOL;
+        echo "3. Pemesanan" . PHP_EOL;
+        echo "4. Pembayaran" . PHP_EOL;
+        echo "5. Detail" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+
+        if($pilihan == "1")
+        {
+            viewShowFood();
+        }else if($pilihan == "2")
+        {
+
+        }else if($pilihan == "3")
+        {
+
+        }else if($pilihan == "4")
+        {
+
+        }else if($pilihan == "5")
+        {
+
+        }else if($pilihan == "x")
+        {
+            break;
+        }else 
+        {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
