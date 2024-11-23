a6c3ef2a795e70b9214cdfaba014b32c88c0889f
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-11-23 17:46:20 +0700

    update App.php require ViewShowDrink.php

--- App.php
+++ App.php
@@ -11,6 +11,7 @@
     require_once __DIR__ . "/View/ViewShowFood.php";
     require_once __DIR__ . "/View/ViewAddFood.php";
     require_once __DIR__ . "/View/ViewRemoveFood.php";
+    require_once __DIR__ . "/View/ViewShowDrink.php";
     require_once __DIR__ . "/Helper/Input.php";
 
     echo "Cafetaria App" . PHP_EOL;
