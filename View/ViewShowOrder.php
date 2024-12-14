d5971c90b2934b0b94bd046ff8173bbb0e88b374
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-14 08:35:45 +0700

    update ViewShowOrder.php menampilkan daftar pemesanan dan menu pilihan

--- View/ViewShowOrder.php
+++ View/ViewShowOrder.php
@@ -1,6 +1,33 @@
 <?php 
 
+    require_once __DIR__ . "/../BusinessLogic/ShowOrder.php";
+    require_once __DIR__ . "/../Helper/Input.php";
+
     function viewShowOrder(): void 
     {
-        
+        while(true)
+        {
+            showOrder();
+
+            echo "Menu Pesanan" . PHP_EOL;
+            echo "1. Pesan Makanan" . PHP_EOL;
+            echo "2. Pesan Minuman" . PHP_EOL;
+            echo "x. Kembali" . PHP_EOL;
+
+            $pilihan = input("Pilih");
+
+            if($pilihan == "1")
+            {
+
+            }else if($pilihan == "2")
+            {
+
+            }else if($pilihan == "x")
+            {
+                break;
+            }else
+            {
+                echo "Pilihan tidak dimengerti" . PHP_EOL;
+            }
+        }
     }
