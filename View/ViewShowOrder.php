6ee50acd3215a5c69af6264062c43afc66b6a84d
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-14 13:46:01 +0700

    update ViewShowOrder.php call viewAddOrder function for drinks order

--- View/ViewShowOrder.php
+++ View/ViewShowOrder.php
@@ -25,7 +25,9 @@
                 $open = false;
             }else if($pilihan == "2")
             {
-
+                $exit = $open ? true : false;
+                viewAddOrder(2, $exit);
+                $open = false;
             }else if($pilihan == "x")
             {
                 break;
