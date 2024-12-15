42082cc4c78aad9a67359a4f168e0fa5a2e5b811
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-15 22:06:23 +0700

    update ViewAddPayment.php menambahkan data pesanan yang telah dibayar ke dalam detail

--- View/ViewAddPayment.php
+++ View/ViewAddPayment.php
@@ -3,6 +3,7 @@
     require_once __DIR__ . "/../Model/Payments.php";
     require_once __DIR__ . "/../BusinessLogic/RemoveOrder.php";
     require_once __DIR__ . "/../BusinessLogic/AddPayment.php";
+    require_once __DIR__ . "/../BusinessLogic/AddDetail.php";
     require_once __DIR__ . "/../Helper/Input.php";
     require_once __DIR__ . "/../Helper/Duplicate.php";
     require_once __DIR__ . "/../Helper/Find.php";
@@ -45,6 +46,7 @@
                     addPayment($code, $pay, $money);
                     $elements = duplicate($code);
                     removeOrder($code);
+                    addDetail($elements);
                     echo "Kembalian : Rp." . $change . PHP_EOL;
                 }
             }else
