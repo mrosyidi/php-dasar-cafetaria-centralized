fded58965f0e642424ccfc92c6f309c5cd0cd97f
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-12-15 21:52:06 +0700

    update ShowDetail.php menampilkan daftar pesanan yang telah diproses

--- BusinessLogic/ShowDetail.php
+++ BusinessLogic/ShowDetail.php
@@ -2,5 +2,23 @@
 
     function showDetail(int $code): void 
     {
+        global $details;
 
+        echo "DAFTAR DETAIL" . PHP_EOL;
+
+        if(empty($details))
+        {
+            echo "Tidak ada daftar detail" . PHP_EOL;
+        }else
+        {
+            foreach($details as $number => $detail)
+            {
+                if($detail['code'] == $code)
+                {
+                    $number++;
+                    echo "$number. " . $detail['code'] . " " . $detail['name'] . " Rp." . $detail['price'] . " (x"
+                    . $detail['qty'] . ") " . $detail['sub_total'] . PHP_EOL;
+                }
+            }
+        }
     }
